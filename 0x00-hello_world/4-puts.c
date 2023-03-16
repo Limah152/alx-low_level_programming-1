261022f5a156b78e96a229acc5195777cc9dc108
Limah152 <limahenterprises152@gmail.com>
2023-03-16 12:02:57 -0700

    4-puts.c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  *  *main - entry point
@@ -7,6 +6,6 @@
  *     */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
