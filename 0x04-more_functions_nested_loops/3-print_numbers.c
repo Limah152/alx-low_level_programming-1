89062f10760e2a6c9c7b130f9e939c4682006432
Limah152 <limahenterprises152@gmail.com>
2023-03-23 15:58:48 -0700

    3 commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_numbers - print numbers
  * Return: Always 0.
@@ -7,12 +8,14 @@ void print_numbers(void)
 {
 int i;
 
+
 i = 0;
+
 while (i < 10)
 {
 _putchar(i);
 i++;
 }
 
-_putchar('\n');
+_putchar(i);
 }
